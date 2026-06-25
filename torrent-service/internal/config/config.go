8c0dfb947e9c3c23d7d12caa16177b757b0a9581
Monstrong <igrachev2006@gmail.com>
2026-06-25 22:10:05 +0300

    задан интерфейс репо

--- torrent-service/internal/config/config.go
+++ torrent-service/internal/config/config.go
@@ -1,5 +1,26 @@
 package config
 
 type Config struct {
-	
+	App struct {
+		Pame string
+		Port int
+	}
+	Postgres struct {
+		Host string
+		Port string
+		User string
+		Password string
+		DBName string
+		maxCon string
+	}
+	Logger struct {
+		Level string 
+	}
+}
+
+
+func Load() (*Config, error) {
+	return &Config{
+
+	}
 }
---
+++ torrent-service/internal/repository/postgres.go
@@ -0,0 +1,18 @@
+package repository
+
+import (
+    "github.com/jackc/pgx/v5/pgxpool"
+    "go.uber.org/zap"
+)
+
+type PostgresRepository struct {
+    db     *pgxpool.Pool
+    logger *zap.Logger
+}
+
+func NewPostgresRepository(db *pgxpool.Pool, logger *zap.Logger) *PostgresRepository{
+	return &PostgresRepository{
+		db:     db,
+		logger: logger,
+	}
+}
