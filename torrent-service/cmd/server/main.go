687c181ec21b19a4c49f9eb6f940b0a5a2509a42
Monstrong <igrachev2006@gmail.com>
2026-07-02 16:14:05 +0300

    logger usage in main.go

--- torrent-service/cmd/server/main.go
+++ torrent-service/cmd/server/main.go
@@ -1,31 +1,41 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/monstrong/gracker2/torrent-service/internal/config"
 	"github.com/monstrong/gracker2/torrent-service/pkg/db"
 	"github.com/monstrong/gracker2/torrent-service/pkg/logger"
+	"go.uber.org/zap"
 )
 
 func main() {
+
+	ctx := context.Background()
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatal("some error loading config")
 	}
 
-	// makes pool and does Ping
-	pool, err := db.NewPool(&cfg.Postgres)
-	if err != nil {
-		log.Fatal("some error connecting db")
-	}
-	defer pool.Close()
-	fmt.Println(pool)
-
-	logger, err := logger.New(&cfg.Logger)
+	l, err := logger.New(&cfg.Logger)
 	if err != nil {
 		log.Fatal("some error creating logger")
 	}
+	l.Info(ctx, "config & logger init complete")
+
+	// makes pool and does Ping
+	pool, err := db.NewPool(&cfg.Postgres)
+	if err != nil {
+		l.Error(ctx, "creating db pool and doing Ping", zap.Error(err))
+		os.Exit(1)
+	}
+	defer pool.Close()
+	l.Info(ctx, "db pool init complete")
+
+
 	
 }
